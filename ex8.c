628034f75f9d3e01d888b84e8f2e04590cd0d3bb
achrafelberkaoui <achrafelberkoaui@gmail.com>
2025-09-02 18:14:01 +0200

    modifier

--- ex8.c
+++ ex8.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 int main(void) {
-    int M ,Billet20 ;
-printf("veuillez entrer le Montant : ");
-scanf("%d",&M);
-Billet20 = 20000 % 6;
-printf("Buillet de 20 $ : %d\n",Billet20);
-
+float M, Ti, Pm,cal;
+printf("veullez entrer le montat de pret : ");
+scanf("%f",&M);
+printf("veullez entrer le taux d'interet de pret : ");
+scanf("%f",&Ti);
+printf("veullez entrer le paiement mensuel : ");
+scanf("%f",&Pm);
+cal = M*(Ti/(100*12))+M;
+for(int i = 0; i<3; i++){
+    cal = cal-Pm;
+    printf("%f \n",cal);
+}
 return 0;
 }
