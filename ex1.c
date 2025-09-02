98c2ecff7ff4f44abbdca0a4b91c9513873ab6ec
achrafelberkaoui <achrafelberkoaui@gmail.com>
2025-09-02 16:23:09 +0200

    modifier numero

--- ex1.c
+++ ex1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void) {
-printf("         *\n");
-printf("        *\n");
-printf("       *\n");
-printf("      *\n");
-printf("     *\n");
-printf("*   *\n");
-printf(" * *\n");
-printf("  *\n");
+printf("1.           *\n");
+printf("2.          *\n");
+printf("3.         *\n");
+printf("4.        *\n");
+printf("5.       *\n");
+printf("6.  *   *\n");
+printf("7.   * *\n");
+printf("8.    *\n");
 return 0;
 }
